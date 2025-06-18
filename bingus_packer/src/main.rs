6957be641b6f756fcbca689dc6d58706c0ed40e1
Afloofe <Afunnyfloof@proton.me>
2025-06-18 14:15:51 +0000

    Added comments and re-did encryption function (not done)

--- bingus_packer/src/main.rs
+++ bingus_packer/src/main.rs
@@ -1,15 +1,21 @@
 use aes_gcm::aead::{Aead, OsRng, rand_core::RngCore};
+use aes_gcm::Aes256Gcm;
+use aes_gcm::AeadCore;
 use std::fs;
 use rand::Rng;
 use std::io;
-
-
+use aes_gcm::Key;
+use aes_gcm::KeyInit;
 fn main() -> io::Result<()> {
-
+    //Ask user for file they wanna pack
     println!("What file would you like to pack?");
-    let file = ask_for_input()?;
+    //Handle error from ask_for_input
+    let file = match ask_for_input(){
+        Err(e) => return Err(e),
+        Ok(file) => file,
+    };
+    //put file into u8 vec
     let data: Vec<u8> = fs::read(file)?;
-    println!("What would you like the key to be? (")
     Ok(())
 } 
 
@@ -20,7 +26,14 @@ fn ask_for_input() -> io::Result<String> {
 }
 
 fn encrypt(file: Vec<u8>) -> Result<Vec<u8>> {
+    //setup encryption variables
+    let key = Aes256Gcm::generate_key(&mut OsRng);
+    let cipher = Aes256Gcm::new(&key);
+    let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
 
-    let mut nonce = [0u8; 12];
-    OsRng.fill_bytes(&mut nonce);
-    
+    let mut buffer: Vec<u8, > = Vec::new();
+    buffer.extend_from_slice(&file);
+
+
+
+}
