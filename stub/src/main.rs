579d0af271866bfc6c1763eb1988af7a6b77e698
Afloofe <Afunnyfloof@proton.me>
2025-06-19 22:13:56 +0000

    Added stub decryption functionality

--- stub/src/main.rs
+++ stub/src/main.rs
@@ -1,6 +1,17 @@
 use aes_gcm::aead::{Aead, AeadCore, Aes256Gcm};
 
-fn main() {
-    let key = PLACEHOLDER;
+fn main() -> io::Result<()> {
+
+    //Get env vars for decryption
+    let key = env::var("KEY");
+    let nonce = env::var("NONCE");
+    
+    //Instantiate cipher and buffer
     let cipher = AES256Gcm::new(&key);
+    let mut buffer: Vec<u8> = Vec::new();
+    
+    //Decrypt
+    cipher.decrypt_in_pace(&nonce, b"", &mut buffer)?;
+    
+
 }
