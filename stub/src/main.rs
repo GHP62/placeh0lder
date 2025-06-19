3fcddf82214d94b5adefab07617fc528f3940d7f
Afloofe <Afunnyfloof@proton.me>
2025-06-19 22:37:06 +0000

    NOT WORKINGgit add . Adding stub

--- stub/src/main.rs
+++ stub/src/main.rs
@@ -1,17 +1,29 @@
 use aes_gcm::aead::{Aead, AeadCore, Aes256Gcm};
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+use std::env;
+use std::io::SeekFrom;
 
 fn main() -> io::Result<()> {
 
     //Get env vars for decryption
-    let key = env::var("KEY");
-    let nonce = env::var("NONCE");
-    
-    //Instantiate cipher and buffer
-    let cipher = AES256Gcm::new(&key);
-    let mut buffer: Vec<u8> = Vec::new();
-    
-    //Decrypt
+    let key = env::var("KEY")?;
+    let nonce = env::var("NONCE")?;
+    let size = env::var("SIZE")?.parse()?; 
+    let filename = env::current_exe()?;
+    let file = File::open(&filename)?;
+    let mut buffer = Vec::new();
+
+    //Read file with offest 
+    file.seek(SeekFrom::End(-1*size));
+    file.read_to_end(&mut buffer)?; 
+
+    //Decrypt 
+    let cipher = Aes256Gcm::new(&key);  
     cipher.decrypt_in_pace(&nonce, b"", &mut buffer)?;
-    
+
+
+    Ok(()) 
 
 }
