ad0cb163f37eccff6cdf03c7f10a2439a448cdf4
Afloofe <Afunnyfloof@proton.me>
2025-06-21 16:21:27 -0500

    im too tired for ts

--- stub/src/main.rs
+++ stub/src/main.rs
@@ -2,22 +2,29 @@ use aes_gcm::Aes256Gcm;
 use aes_gcm::KeyInit;
 use aes_gcm::aead::{AeadInPlace, Aead, AeadCore, generic_array::GenericArray};
 use aes_gcm::Nonce;
+use aes_gcm::Key;
 use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 use std::env;
 use std::io::SeekFrom;
+use aes::cipher::generic_array::{typenum::U16};
 
 fn main() -> io::Result<()> {
-
+    match env::var("key") {
+        Ok(key) => key,
+        Err(e) => e,
+    };
     //Get env vars for decryption
-    let key = env::var("KEY").expect("");
-    let nonce = env::var("NONCE").expect("");
-    let size = env::var("SIZE").expect("").parse()?; 
+    let key = Key::from_slice(&env::var("KEY").expect("key").as_bytes());
+    let nonce = Nonce::from_slice(&env::var("NONCE").expect("nonce").as_bytes()); 
+    let size = env::var("SIZE")?; 
     let filename = env::current_exe()?;
     let file = File::open(&filename)?;
     let mut buffer = Vec::new();
-
+    
+    
+    
     //Read file with offest 
     file.seek(SeekFrom::End(-1*size));
     file.read_to_end(&mut buffer)?; 
@@ -29,4 +36,9 @@ fn main() -> io::Result<()> {
 
     Ok(()) 
 
+}
+
+fn string_to_key(string: &String) -> io::Result<()>  {
+    let string_bytes = string.as_bytes();
+    return Key::<Aes256Gcm>::from_slice(&string_bytes).clone();
 }
