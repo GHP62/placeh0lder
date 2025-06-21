ed469823c2b4ccb5f40706c45807d97143672f49
Afloofe <Afunnyfloof@proton.me>
2025-06-21 00:12:06 +0000

    Main

--- stub/src/main.rs
+++ stub/src/main.rs
@@ -1,4 +1,7 @@
-use aes_gcm::aead::{Aead, AeadCore, Aes256Gcm};
+use aes_gcm::Aes256Gcm;
+use aes_gcm::KeyInit;
+use aes_gcm::aead::{AeadInPlace, Aead, AeadCore, generic_array::GenericArray};
+use aes_gcm::Nonce;
 use std::io;
 use std::io::prelude::*;
 use std::fs::File;
@@ -8,9 +11,9 @@ use std::io::SeekFrom;
 fn main() -> io::Result<()> {
 
     //Get env vars for decryption
-    let key = env::var("KEY")?;
-    let nonce = env::var("NONCE")?;
-    let size = env::var("SIZE")?.parse()?; 
+    let key = env::var("KEY").expect("");
+    let nonce = env::var("NONCE").expect("");
+    let size = env::var("SIZE").expect("").parse()?; 
     let filename = env::current_exe()?;
     let file = File::open(&filename)?;
     let mut buffer = Vec::new();
@@ -21,7 +24,7 @@ fn main() -> io::Result<()> {
 
     //Decrypt 
     let cipher = Aes256Gcm::new(&key);  
-    cipher.decrypt_in_pace(&nonce, b"", &mut buffer)?;
+    cipher.decrypt_in_place(&nonce, b"", &mut buffer);
 
 
     Ok(()) 
